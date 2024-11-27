3a5b1a0d016ea37d04cb7b7bd36c9be4ef4bd63d
junho3168 <junho3168@naver.com>
2024-11-27 16:50:25 +0900

    practice

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,13 +1,16 @@
 package com.ll;
 
+import java.io.*;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        lab1();
-        lab2();
+        //lab1();
+        //lab2();
+        //lab3();
+        lab4();
     }
 
     private static void lab1() {
@@ -18,7 +21,52 @@ public class Main {
 
         System.out.println("입력한 명령: " + cmd);
     }
-    private static void lab2() {
 
+    private static void lab2() {
+        InputStream in = new ByteArrayInputStream("안녕\n잘가".getBytes());
+        Scanner scanner = new Scanner(in);
+
+        System.out.println("명령) ");
+        String cmd = scanner.nextLine().trim();
+
+        System.out.println("입력한 명령: " + cmd);
+
+        System.out.println("명령) ");
+        cmd = scanner.nextLine().trim();
+
+        System.out.println("입력한 명령: " + cmd);
+
+    }
+
+    private static void lab3() {
+        System.out.println("== 시작 ==");
+
+        System.out.println("안녕하세요");
+
+        System.out.println("== 끝 ==");
+    }
+
+    private static void lab4() {
+
+        System.out.println("== 시작 ==");
+        //출력이 모니터로 안되도록 설정
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        System.out.println("안녕하세요");
+        System.out.println("반갑습니다");
+
+        //다시 모니터로 출력되도록 설정
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+
+        System.out.println("== 끝 ==");
+
+        System.out.println("출력 : " + output.toString());//output.toString으로 모을 수 있다
+        //out 자원 해제
+        try {
+            output.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
